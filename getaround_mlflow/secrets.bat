770cf0a04db3356126f01855ead10c85cc96f1e0
sdupland <133764524+sdupland@users.noreply.github.com>
2024-01-13 15:58:17 +0100

    Delete getaround_mlflow/secrets.bat

--- getaround_mlflow/secrets.bat
+++
@@ -1,4 +0,0 @@
-$env:AWS_ACCESS_KEY_ID=Axxxxxxxxxxxxxxxxxxxxxxxxx
-$env:AWS_SECRET_ACCESS_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-$env:BACKEND_STORE_URI=postgresql://avawfkylyjrphx:61a91302ae3320d6d94b867ff2d474754f58a3466eb2e3244140c81872fa01dc@ec2-3-217-146-37.compute-1.amazonaws.com:5432/dc1i7l944ogagv
-$env:ARTIFACT_STORE_URI=s3://mlflow-getaround-bucket/getaround/
