009dc9d0c404f3eeb36891f57aa2763e7548d9f1
sdupland <133764524+sdupland@users.noreply.github.com>
2024-01-13 15:55:22 +0100

    Add files via upload

--- getaround_mlflow/app_mlflow.py
+++ getaround_mlflow/app_mlflow.py
@@ -1,11 +1,11 @@
 # import necessary libraries
 import pandas as pd
+
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-# from sklearn.metrics import r2_score, mean_squared_error, accuracy_score
 import xgboost as xgb
 
 import mlflow
@@ -20,7 +20,7 @@ Y = dataset_price_clean.iloc[:,-1:]
 numeric_features = []
 categorical_features = []
 for i,t in X.dtypes.items() :
-    if ('float' in str(t)) or ('int' in str(t)) :
+    if ("float" in str(t)) or ("int" in str(t)) :
         numeric_features.append(i)
     else :
         categorical_features.append(i)
@@ -28,23 +28,26 @@ for i,t in X.dtypes.items() :
 # Create pipeline for numeric features
 numeric_transformer = Pipeline(steps=[
     ("Imputer", SimpleImputer(strategy="median")), # not necessary but in case of
-    ('scaler', StandardScaler())
+    ("scaler", StandardScaler())
 ])
 
 # Create pipeline for categorical features
 categorical_transformer = Pipeline(steps=[
     ("Imputer", SimpleImputer(strategy="most_frequent")), # not necessary but in case of
-    ('encoder', OneHotEncoder(drop='first')) # first column will be dropped to avoid creating correlations between features
+    ("encoder", OneHotEncoder(drop="first")) # first column will be dropped to avoid creating correlations between features
     ])
 
 # Use ColumnTransformer to make a preprocessor object that describes all the treatments to be done
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', numeric_transformer, numeric_features),
-        ('cat', categorical_transformer, categorical_features)
+        ("num", numeric_transformer, numeric_features),
+        ("cat", categorical_transformer, categorical_features)
     ])
 
+# create two datasets from X and Y, with 80% for training and 20% for testing
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=0)
+
+# preprocess features
 X_train = preprocessor.fit_transform(X_train)
 X_test = preprocessor.transform(X_test)
 
@@ -53,9 +56,9 @@ EXPERIMENT_NAME="my-first-mlflow-experiment"
 
 # instanciate your experiment
 client=mlflow.tracking.MlflowClient()
-mlflow.set_tracking_uri("https://getaround-mlflow-ed95616b9f3c.herokuapp.com/")
+mlflow.set_tracking_uri("https://getaround-mlflow-f2520114f48c.herokuapp.com")
 
-# Set experiment's info 
+# Set experiment"s info 
 mlflow.set_experiment(EXPERIMENT_NAME)
 
 # Get our experiment info
@@ -65,22 +68,24 @@ experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
 mlflow.xgboost.autolog()
 
 with mlflow.start_run(experiment_id = experiment.experiment_id):
-    
-    params_grid = {
-        "learning_rate": [0.05,0.1,0.15],
-        "max_depth": [2,4,6,8],
-        "n_estimators" : [50,150,250]
-    }
  
-    # Instanciate and fit the model
-    xgb_regressor = xgb.XGBRegressor(random_state=0)
-    model = GridSearchCV(xgb_regressor, params_grid, n_jobs=-1, cv=4)
-
-    model.fit(X_train, Y_train)
+    # Instanciate and fit the model with gridsearchcv (cross validation + try of differents values of parameters, cf params_grid)
+    xgb_regressor = xgb.XGBRegressor(random_state=0,learning_rate=0.1, max_depth=5, n_estimators=150)
+    xgb_regressor.fit(X_train, Y_train)
 
     # Store metrics
-    predicted_price = model.predict(X_test)
-    score = model.score(X_test, Y_test)
+    predicted_price = xgb_regressor.predict(X_test)
+    score = xgb_regressor.score(X_test, Y_test)
+    
+    # Log Metric 
+    # mlflow.log_metric("Accuracy", score)
+    
+    # log best parameters
+    #importance = model.best_params_(importance_type='weight')
+    # mlflow.log_artifact(importance, 'feature_importance.json')
+    
+    # Log model 
+    # mlflow.sklearn.log_model(model, "model")
 
     # Print results 
     print("XGBoost model")
